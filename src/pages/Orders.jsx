b60ae32eeb9f7ba78c2254a6f31221f4d45388e4
vanh1604 <VietAnhScout@gmail.com>
2025-02-25 23:11:55 +0700

    last fix

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
-import { backendUrl } from "./../../../admin/src/App";
 import axios from "axios";
 
 const Orders = () => {
